179084baf30fdc6d7da9d53c9eef9c6c9762f508
Gustavo Marques <gmarques@ucar.edu>
2020-05-28 09:11:07 -0600

    Workaround to avoid overflows in string functions

--- src/ALE/MOM_regridding.F90
+++ src/ALE/MOM_regridding.F90
@@ -313,8 +313,8 @@ subroutine initialize_regridding(CS, GV, US, max_depth, param_file, mdl, coord_m
     dz(:) = uniformResolution(ke, coord_mode, tmpReal, &
                               US%R_to_kg_m3*(GV%Rlay(1) + 0.5*(GV%Rlay(1)-GV%Rlay(min(2,ke)))), &
                               US%R_to_kg_m3*(GV%Rlay(ke) + 0.5*(GV%Rlay(ke)-GV%Rlay(max(ke-1,1)))) )
-    if (main_parameters) call log_param(param_file, mdl, "!"//coord_res_param, dz, &
-                   trim(message), units=trim(coord_units))
+!    if (main_parameters) call log_param(param_file, mdl, "!"//coord_res_param, dz, &
+!                   trim(message), units=trim(coord_units))
   elseif (trim(string)=='PARAM') then
     ! Read coordinate resolution (main model = ALE_RESOLUTION)
     ke = GV%ke ! Use model nk by default
@@ -381,13 +381,13 @@ subroutine initialize_regridding(CS, GV, US, max_depth, param_file, mdl, coord_m
       call MOM_error(FATAL,trim(mdl)//', initialize_regridding: '// &
                  'Mismatch in number of model levels and "'//trim(string)//'".')
     endif
-    if (main_parameters) call log_param(param_file, mdl, "!"//coord_res_param, dz, &
-               trim(message), units=coordinateUnits(coord_mode))
+!    if (main_parameters) call log_param(param_file, mdl, "!"//coord_res_param, dz, &
+!               trim(message), units=coordinateUnits(coord_mode))
   elseif (index(trim(string),'FNC1:')==1) then
     ke = GV%ke; allocate(dz(ke))
     call dz_function1( trim(string(6:)), dz )
-    if (main_parameters) call log_param(param_file, mdl, "!"//coord_res_param, dz, &
-               trim(message), units=coordinateUnits(coord_mode))
+!    if (main_parameters) call log_param(param_file, mdl, "!"//coord_res_param, dz, &
+!               trim(message), units=coordinateUnits(coord_mode))
   elseif (index(trim(string),'RFNC1:')==1) then
     ! Function used for set target interface densities
     ke = rho_function1( trim(string(7:)), rho_target )
@@ -412,8 +412,8 @@ subroutine initialize_regridding(CS, GV, US, max_depth, param_file, mdl, coord_m
       call MOM_read_data(trim(fileName), trim(varName), dz)
     endif
     if (main_parameters) then
-      call log_param(param_file, mdl, "!"//coord_res_param, dz, &
-               trim(message), units=coordinateUnits(coord_mode))
+!      call log_param(param_file, mdl, "!"//coord_res_param, dz, &
+!               trim(message), units=coordinateUnits(coord_mode))
       call log_param(param_file, mdl, "!TARGET_DENSITIES", rho_target, &
                'HYBRID target densities for interfaces', units=coordinateUnits(coord_mode))
     endif
